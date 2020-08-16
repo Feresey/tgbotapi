e63b475e106ad7ef6616a772b19a94ec363165db
Feresey <p.milko1999@yandex.ru>
2020-08-16 23:26:25 +0300

    Init enums

---
+++ api/api.go
@@ -0,0 +1,12 @@
+package api
+
+import "encoding/json"
+
+type APIResponse struct {
+	OK          bool                `json:"ok"`
+	Result      json.RawMessage     `json:"result"`
+	Description *string             `json:"description,omitempty"`
+	Parameters  *ResponseParameters `json:"parameters,omitempty"`
+}
+
+// func (m*MessageEntity) Type
